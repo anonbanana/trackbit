ae54ea2027d3ac5afbeced6205a5703a334df680
TrackBit Fix <trackbit@fix>
2026-07-26 09:41:40 +0700

    fix: force Kotlin 2.0 for Sentry compatibility

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,19 +5,30 @@ allprojects {
     }
 }
 
+subprojects {
+    afterEvaluate {
+        if (plugins.hasPlugin("org.jetbrains.kotlin.android")) {
+            extensions.configure<org.jetbrains.kotlin.gradle.dsl.KotlinAndroidExtension> {
+                compilerOptions {
+                    languageVersion.set(org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_2_0)
+                    apiVersion.set(org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_2_0)
+                }
+            }
+        }
+    }
+    val newSubprojectBuildDir: Directory = rootProject.layout.buildDirectory
+        .dir("../../build")
+        .get()
+        .dir(project.name)
+    project.layout.buildDirectory.value(newSubprojectBuildDir)
+    project.evaluationDependsOn(":app")
+}
+
 val newBuildDir: Directory =
     rootProject.layout.buildDirectory
         .dir("../../build")
         .get()
 rootProject.layout.buildDirectory.value(newBuildDir)
-
-subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
-}
-subprojects {
-    project.evaluationDependsOn(":app")
-}
 
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
